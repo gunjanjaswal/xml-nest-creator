3ee01b9d12e959916578df6431d7d52a8d05e9ee
Gunjan Jaswal <gunjanjaswal@gmail.com>
2026-08-24 16:12:53 +0530

    Bump Tested up to WordPress 7.1

--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: gunjanjaswal
 Tags: simple xml generator, simple sitemap, xml generator, sitemap, seo
 Requires at least: 5.5
-Tested up to: 7.0
+Tested up to: 7.1
 Stable tag: 1.1.0
 Requires PHP: 7.4
 License: GPLv2 or later
--- xml-nest-creator.php
+++ xml-nest-creator.php
@@ -8,7 +8,7 @@
  * Author URI:  https://www.gunjanjaswal.me
  * Text Domain: xml-nest-creator
  * Requires at least: 5.5
- * Tested up to: 7.0
+ * Tested up to: 7.1
  * Requires PHP: 7.4
  * License:     GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
